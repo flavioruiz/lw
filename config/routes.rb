51c81d549953b67643cfc26c86c5dd01afaea4b7
flavioruiz <flavioruiz@gmail.com>
2014-01-02 15:46:47 -0300

    initial commit for deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Lw::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+
+  get '/theme', to: "home#theme"
 end
